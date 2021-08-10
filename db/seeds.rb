6cd81ce6b350d6ac4394020ab5ab8de450a1bf7d
joao sampaio <joaomarcosemgp@gmail.com>
2021-08-10 18:43:55 -0300

    modify views

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Video.destroy_all
+
+Video.create!(
+  title: 'Light & Spacious Garden Video London',
+  url: 'https://images.unsplash.com/photo-1494526585095-c41746248156?w=1200'
+)
+
+Video.create!(
+  title: 'Stylish House Close to River Thames',
+  url: 'https://images.unsplash.com/photo-1493809842364-78817add7ffb?w=1200'
+)
+
+Video.create!(
+  title: 'St Pancras Clock Tower Guest Suite',
+  url: 'https://images.unsplash.com/photo-1502672260266-1c1ef2d93688?w=1200'
+)
